8f12b943daebfa325c42187f01b4c1c3616b89c2
Jonas Jensen <jonasje@axis.com>
2023-09-29 15:40:07 +0200

    Replace shmat with mapped_region

--- boost-anonymous_shared_memory.cpp
+++ boost-anonymous_shared_memory.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <boost/interprocess/managed_external_buffer.hpp>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/xsi_shared_memory.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/managed_external_buffer.hpp>
 
 void WriteStringToSharedMemory(const char* message) {
-	boost::interprocess::xsi_shared_memory xsm(boost::interprocess::open_or_create, {"/tmp/", 1}, 10000);
-    boost::interprocess::managed_external_buffer extBuffer(boost::interprocess::open_only, (void*)(xsm.get_mapping_handle().handle), 10000);
+	boost::interprocess::xsi_key key("/tmp/", 1);
+	std::cout << "WriteStringToSharedMemory! key=" << key.get_key() << std::endl;
+	boost::interprocess::xsi_shared_memory xsm(boost::interprocess::open_or_create, key, 10000, 0640);
+	boost::interprocess::mapped_region mr(xsm, boost::interprocess::read_write);
+    boost::interprocess::managed_external_buffer extBuffer(boost::interprocess::create_only, mr.get_address(), mr.get_size());
 
     auto sharedString = extBuffer.find_or_construct<boost::interprocess::basic_string<char>>("MyString")();
     sharedString->assign(message);
     std::cout << "String '" << message << "' written to shared memory." << std::endl;
-
-    shmdt(reinterpret_cast<void*>(xsm.get_mapping_handle().handle));
 }
 
 void ReadStringFromSharedMemory() {
-	boost::interprocess::xsi_shared_memory xsm(boost::interprocess::open_only, {"/tmp/", 1});
-    boost::interprocess::managed_external_buffer extBuffer(boost::interprocess::open_only, reinterpret_cast<void*>(xsm.get_mapping_handle().handle), 10000);
+	boost::interprocess::xsi_key key("/tmp/", 1);
+	std::cout << "ReadStringFromSharedMemory! key=" << shmget(key.get_key(), 0, 0)  << std::endl;
+	boost::interprocess::xsi_shared_memory xsm(boost::interprocess::open_or_create, key, 10000, 0400);
+	boost::interprocess::mapped_region mr(xsm, boost::interprocess::read_only);
+    boost::interprocess::managed_external_buffer extBuffer(boost::interprocess::open_only, mr.get_address(), mr.get_size());
 
     auto sharedString = extBuffer.find<boost::interprocess::basic_string<char>>("MyString").first;
     if (!sharedString) {
         std::cerr << "String not found in shared memory." << std::endl;
         return;
     }
-    std::cout << "String read from shared memory: " << *sharedString << std::endl;
 
-    shmdt(reinterpret_cast<void*>(xsm.get_mapping_handle().handle));
+    std::cout << "String read from shared memory: " << *sharedString << std::endl;
 }
 
 int main(int argc, char* argv[]) {
