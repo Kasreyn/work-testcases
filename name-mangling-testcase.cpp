35db5290dbc62e5bc9e6956fc203b44225ba481f
Jonas Jensen <jonasje@axis.com>
2023-10-20 11:45:14 +0200

    Kort text demo av versioning.. clang++ vs g++. Det blir samma "mangling" i båda, och samma flaggor in till kompilatorn ..
    Notera "@@" efter symbolens namn, det är default symbolen. Vid det tillfället en klient kompilerar sig själv mot vårt bibliotek så kommer den resulterande binären specifikt leta efter den symbolen vid uppstart.
    
    Notera text strängen SONAME "name-mangling-testcase.so.1" (inte filen alltså) som hamnar i resulterande .so, klienten kommer att leta efter symboler i bibliotek med det namnet, major måste alltså vara ".1", om det saknas en fil som har rätt SONAME så kan klienten inte starta alls! Man kan säga att så länge det är samma SONAME så förväntas biblioteket vara bakåtkompatibelt..
    
    Tekniskt innebär det att vi bör flytta major vid nästa release när vi inte planerar på att vara bakåtkompatibel, samma major om vi syftar på att vara kompatibel.  Så jag föreslår .. Panorama's ACAP ska använda ".0" eller ".1" men eftersom vi planerar inkompatibla ändringar i API så blir nästa ".1" eller ".2". Men bra om vi kan synka vilken major som används redan nu, master har just nu ".0" som major.
    
    clang++-13 -Wl,--soname,name-mangling-testcase.so.1 -fPIC -shared -o name-mangling-testcase.so name-mangling-testcase.cpp
    nm -D name-mangling-testcase.so | grep Setup
    0000000000001330 W _ZN6Person5SetupEv
    
    g++ -Wl,--soname,name-mangling-testcase.so.1 -fPIC -shared -o name-mangling-testcase.so name-mangling-testcase.cpp
    nm -D name-mangling-testcase.so | grep Setup
    000000000000131c W _ZN6Person5SetupEv
    
    cat ct.map
    CTL_1.0 { global: _ZN6Person5SetupEv; local: *; };
    
    clang++-13 -Wl,--soname,name-mangling-testcase.so.1 -fPIC -shared -o name-mangling-testcase.so name-mangling-testcase.cpp -Wl,-version-script,ct.map
    nm -D name-mangling-testcase.so | grep Setup
    0000000000001310 W _ZN6Person5SetupEv@@CTL_1.0
    
    g++ -Wl,--soname,name-mangling-testcase.so.1 -fPIC -shared -o name-mangling-testcase.so name-mangling-testcase.cpp -Wl,-version-script,ct.map
    nm -D name-mangling-testcase.so | grep Setup
    00000000000012fc W _ZN6Person5SetupEv@@CTL_1.0
    
    readelf -d name-mangling-testcase.so | grep SONAME
    0x000000000000000e (SONAME)             Library soname: [name-mangling-testcase.so.1]

---
+++ name-mangling-testcase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Person {
+public:
+    Person(const std::string& name) : name(name) {}
+
+    void Setup() {
+        std::cout << "Hello, my name is " << name << std::endl;
+    }
+
+private:
+    std::string name;
+};
+
+int main() {
+    Person person("John");
+    person.Setup();
+
+    return 0;
+}
+
