2134925508181424156b2eae1aae0ed2fec3707f
Jonas Jensen <jonasje@axis.com>
2023-12-13 13:12:19 +0100

    std::pair bool reference return

--- return-dangling-reference.cpp
+++ return-dangling-reference.cpp
@@ -1,26 +1,27 @@
-#include <unordered_map>
-#include <iostream>
-
-#define KV_PAIR(key, value) {key, value}
-
-std::unordered_map<int, std::string> myMap{
-	KV_PAIR(1, "One Thousand"),
-	KV_PAIR(2, "One Thousand")
-};
-
-std::string& GetString(int ID) {
-	if (!myMap.contains(ID)) {
-		return *(std::string*)nullptr;
+	#include <unordered_map>
+	#include <iostream>
+	
+	std::unordered_map<int, std::string> myMap{
+		{1, "One Thousand"},
+		{2, "One Thousand"}
+	};
+	
+	std::pair<std::string&, bool> GetString(int ID) {
+		if (!myMap.contains(ID)) {
+			return {*(std::string*)nullptr, false};
+		}
+		return {myMap.at(ID), true};
 	}
-
-	return myMap.at(ID);
-}
-
-int main(int argc, char *argv[]) {
-	std::string& s1 = GetString(1);
-	std::cout << s1 << std::endl;
-
-	std::string& s2 = GetString(3);
-
-	return 0;
-}
+	
+	int main(int argc, char *argv[]) {
+		std::pair<std::string&, bool> p1 = GetString(1);
+		std::pair<std::string&, bool> p2 = GetString(2);
+		if (!p1.second || !p2.second) {
+			return 1;
+		}
+		std::string& s1 = p1.first;
+		std::string& s2 = p2.first;
+		std::cout << s1 << std::endl;
+		std::cout << s2 << std::endl;
+		return 0;
+	}
