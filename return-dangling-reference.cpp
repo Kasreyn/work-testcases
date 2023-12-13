c097c42e3df1e90534893ba70c3899fac0ff8681
Jonas Jensen <jonasje@axis.com>
2023-12-13 16:15:37 +0100

    Remove pair

--- return-dangling-reference.cpp
+++ return-dangling-reference.cpp
@@ -2,26 +2,26 @@
 	#include <iostream>
 	
 	std::unordered_map<int, std::string> myMap{
-		{1, "One Thousand"},
-		{2, "One Thousand"}
+	    {1, "One Thousand"},
+	    {2, "One Thousand"}
 	};
 	
-	std::pair<std::string*, bool> GetString(int ID) {
-		if (!myMap.contains(ID)) {
-			return {nullptr, false};
-		}
-		return {&myMap.at(ID), true};
+	std::string* GetString(int ID) {
+	    if (!myMap.contains(ID)) {
+	        return nullptr;
+	    }
+	    return &myMap.at(ID);
 	}
 	
 	int main(int argc, char *argv[]) {
-		std::pair<std::string*, bool> p1 = GetString(1);
-		std::pair<std::string*, bool> p2 = GetString(2);
-		if (!p1.second || !p2.second) {
-			return 1;
-		}
-		std::string& s1 = *p1.first;
-		std::string& s2 = *p2.first;
-		std::cout << s1 << std::endl;
-		std::cout << s2 << std::endl;
-		return 0;
+	    std::string* p1 = GetString(1);
+	    std::string* p2 = GetString(2);
+	    if (!p1 || !p2) {
+	        return 1;
+	    }
+	    std::string& s1 = *p1;
+	    std::string& s2 = *p2;
+	    std::cout << s1 << std::endl;
+	    std::cout << s2 << std::endl;
+	    return 0;
 	}
