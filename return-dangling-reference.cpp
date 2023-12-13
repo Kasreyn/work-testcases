d8ef3b5dd8162586272f07e9bf952acc6e334d86
Jonas Jensen <jonasje@axis.com>
2023-12-13 13:26:21 +0100

    Dangling pointer return

--- return-dangling-reference.cpp
+++ return-dangling-reference.cpp
@@ -6,21 +6,21 @@
 		{2, "One Thousand"}
 	};
 	
-	std::pair<std::string&, bool> GetString(int ID) {
+	std::pair<std::string*, bool> GetString(int ID) {
 		if (!myMap.contains(ID)) {
-			return {*(std::string*)nullptr, false};
+			return {nullptr, false};
 		}
-		return {myMap.at(ID), true};
+		return {&myMap.at(ID), true};
 	}
 	
 	int main(int argc, char *argv[]) {
-		std::pair<std::string&, bool> p1 = GetString(1);
-		std::pair<std::string&, bool> p2 = GetString(2);
+		std::pair<std::string*, bool> p1 = GetString(1);
+		std::pair<std::string*, bool> p2 = GetString(2);
 		if (!p1.second || !p2.second) {
 			return 1;
 		}
-		std::string& s1 = p1.first;
-		std::string& s2 = p2.first;
+		std::string& s1 = *p1.first;
+		std::string& s2 = *p2.first;
 		std::cout << s1 << std::endl;
 		std::cout << s2 << std::endl;
 		return 0;
