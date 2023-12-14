4e4e5607a096f9756d13aed28deb74afccce7d15
Jonas Jensen <jonasje@axis.com>
2023-12-14 09:04:27 +0100

    Introduce an exception class

--- return-dangling-reference.cpp
+++ return-dangling-reference.cpp
@@ -1,27 +1,34 @@
-	#include <unordered_map>
-	#include <iostream>
-	
-	std::unordered_map<int, std::string> myMap{
-	    {1, "One Thousand"},
-	    {2, "One Thousand"}
-	};
-	
-	std::string* GetString(int ID) {
-	    if (!myMap.contains(ID)) {
-	        return nullptr;
-	    }
-	    return &myMap.at(ID);
+#include <unordered_map>
+#include <iostream>
+
+class FindException : public std::runtime_error {
+	public:
+		FindException(const std::string& message) : std::runtime_error(message) {}
+};
+
+using MyMap = std::unordered_map<int, std::string>;
+MyMap myMap{
+	{1, "One Thousand"},
+	{2, "One Thousand"}
+};
+
+std::string* GetString(int ID) {
+	MyMap::const_iterator it = myMap.find(ID);
+	if (it == myMap.end()) {
+		return nullptr;
 	}
-	
-	int main(int argc, char *argv[]) {
-	    std::string* p1 = GetString(1);
-	    std::string* p2 = GetString(2);
-	    if (!p1 || !p2) {
-	        return 1;
-	    }
-	    std::string& s1 = *p1;
-	    std::string& s2 = *p2;
-	    std::cout << s1 << std::endl;
-	    std::cout << s2 << std::endl;
-	    return 0;
+	return &myMap.at(ID);
+}
+
+int main(int argc, char *argv[]) {
+	const std::string* s1 = GetString(1);
+	const std::string* s2 = GetString(2);
+	if (!s1 || !s2) {
+		return 1;
 	}
+	const std::string& r1 = *s1;
+	const std::string& r2 = *s2;
+	std::cout << r1 << std::endl;
+	std::cout << r2 << std::endl;
+	return 0;
+}
