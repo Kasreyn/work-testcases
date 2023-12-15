57ee0ac19766043bd678b96fb2be9da08dd4c025
Jonas Jensen <jonasje@axis.com>
2023-12-15 09:10:48 +0100

    FindException testcase

--- return-dangling-reference.cpp
+++ return-dangling-reference.cpp
@@ -1,34 +1,36 @@
-#include <unordered_map>
-#include <iostream>
-
-class FindException : public std::runtime_error {
-	public:
-		FindException(const std::string& message) : std::runtime_error(message) {}
-};
-
-using MyMap = std::unordered_map<int, std::string>;
-MyMap myMap{
-	{1, "One Thousand"},
-	{2, "One Thousand"}
-};
-
-std::string* GetString(int ID) {
-	MyMap::const_iterator it = myMap.find(ID);
-	if (it == myMap.end()) {
-		return nullptr;
+	#include <unordered_map>
+	#include <iostream>
+	
+	class FindException : public std::runtime_error {
+		public:
+			FindException(const std::string& message) : std::runtime_error(message) {}
+	};
+	
+	using MyMap = std::unordered_map<int, std::string>;
+	
+	MyMap myMap{
+		{1, "One Thousand"},
+		{2, "One Thousand"}
+	};
+	
+	const std::string& GetString(int ID) {
+		MyMap::const_iterator it = myMap.find(ID);
+		if (it == myMap.end()) {
+			throw FindException("GetString failed to find ID");
+		}
+		return it->second;
 	}
-	return &myMap.at(ID);
-}
-
-int main(int argc, char *argv[]) {
-	const std::string* s1 = GetString(1);
-	const std::string* s2 = GetString(2);
-	if (!s1 || !s2) {
-		return 1;
+	
+	int main(int argc, char *argv[]) {
+		try {
+			const std::string& s1 = GetString(1);
+			const std::string& s2 = GetString(3);
+			std::cout << s1 << std::endl;
+			std::cout << s2 << std::endl;
+		}
+		catch (FindException& e) {
+			std::cout << e.what() << std::endl;
+			return 1;
+		}
+		return 0;
 	}
-	const std::string& r1 = *s1;
-	const std::string& r2 = *s2;
-	std::cout << r1 << std::endl;
-	std::cout << r2 << std::endl;
-	return 0;
-}
