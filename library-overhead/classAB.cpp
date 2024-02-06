21ba88789dc5f95059f2f94ad68cb60b9db25769
Jonas Jensen <jonasje@axis.com>
2024-02-06 15:40:35 +0100

    Remove boost::asio from class B

--- library-overhead/classAB.cpp
+++ library-overhead/classAB.cpp
@@ -2,18 +2,16 @@
 #include <boost/asio/local/stream_protocol.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 
-class B {
-	public:
-		std::string m_Name = "B";
-		boost::asio::io_context m_ioContext;
-		boost::asio::local::stream_protocol::socket m_Socket{m_ioContext};
-};
-
 class A {
 	public:
 		std::string m_Name = "A";
 		boost::asio::io_context m_ioContext;
 		boost::asio::local::stream_protocol::socket m_Socket{m_ioContext};
+};
+
+class B {
+	public:
+		std::string m_Name = "B";
 };
 
 int main(int argc, char *argv[]) {
--- library-overhead/classB.cpp
+++ library-overhead/classB.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <boost/asio/local/stream_protocol.hpp>
-#include <boost/asio/posix/stream_descriptor.hpp>
 
 class B {
 	public:
 		std::string m_Name = "B";
-		boost::asio::io_context m_ioContext;
-		boost::asio::local::stream_protocol::socket m_Socket{m_ioContext};
 };
 
 int main(int argc, char *argv[]) {
