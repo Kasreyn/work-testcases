a925a3ed08dfc55be580643426bb42999c7f4961
Jonas Jensen <jonasje@axis.com>
2024-11-15 15:16:34 +0100

    Add 'namespace __gnu_debug' 'struct' 'struct sched_param' filters

--- filterPP.js
+++ filterPP.js
@@ -1,3 +1,5 @@
+// g++ -E -P -std=c++20 -I. -isystem oe-workdir/recipe-sysroot-native/ Common/Spaces-source.hpp | node ~/stash/testcases/filterPP.js | clang-format | less
+
 const fs = require('fs');
 
 function removeLineDirectives(input) {
@@ -19,7 +21,7 @@ function removeEmptyLines(input) {
 	return filtered.join('\n');
 }
 
-function removeNamespaces(input) {
+function removeNamespaces1(input) {
 	let insideNamespace = false;
 	let braceDepth = 0;
 	const lines	= input.split('\n');
@@ -37,11 +39,7 @@ function removeNamespaces(input) {
         if (line.includes('namespace std') || line.includes('namespace boost') || line.includes('namespace __gnu_cxx') || line.includes('extern') || line.includes('__gthread') || line.includes('namespace __pstl') || line.includes('namespace mpl_') || line.includes('namespace __cxxabiv1') /* || line.includes('enum') || line.includes('struct sched_param') || line.includes('struct timex')*/ /* || line.includes('namespace __gnu_debug')*/) {
             insideNamespace = true;
             braceDepth = (line.match(/{/g) || []).length - (line.match(/}/g) || []).length;
-/*
-            if (braceDepth === 0) {
-                insideNamespace = false;
-            }
-*/
+
             return false;
         }
 
@@ -50,6 +48,35 @@ function removeNamespaces(input) {
 
 	return filtered.join('\n');
 }
+
+function removeNamespaces2(input) {
+	let insideNamespace = false;
+	let braceDepth = 0;
+	const lines	= input.split('\n');
+
+	const filtered = lines.filter(line => {
+		if (insideNamespace) {
+            braceDepth += (line.match(/{/g) || []).length - (line.match(/}/g) || []).length;
+            if (braceDepth === 0) {
+                insideNamespace = false;
+                return false;
+            }
+            return false;
+        }
+
+        if (line.includes('namespace __gnu_debug') || line.includes('struct') || line.includes('struct sched_param')) {
+            insideNamespace = true;
+            braceDepth = (line.match(/{/g) || []).length - (line.match(/}/g) || []).length;
+
+            return false;
+        }
+
+		return !insideNamespace;
+	});
+
+	return filtered.join('\n');
+}
+
 
 function removePragmaDirectives(input) {
 	const lines	   = input.split('\n');
@@ -129,11 +156,12 @@ function applyFilters(input) {
 	let output = removeLineDirectives(input);
 	output	   = removeStdHeaders(output);
 	output	   = removeEmptyLines(output);
-	output	   = removeNamespaces(output);
+	output	   = removeNamespaces1(output);
 	output	   = removePragmaDirectives(output);
 	output	   = removeTypedefStructDirectives(output);
 	output	   = removeTypedefDirectives(output);
 	output     = removeBraceContentAfterNamespace(output);
+	output	   = removeNamespaces2(output);
 	return output;
 }
 
