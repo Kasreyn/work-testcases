6e17d25f3d824cede3e5d9c8fe1a939ea896d1d3
Jonas Jensen <jonasje@axis.com>
2025-05-19 15:22:25 +0200

    clang++-19 -std=c++20 -I. -Wall -Wextra -Wno-unused-command-line-argument -Xanalyzer -analyzer-checker=alpha.cplusplus.DeleteWithNonVirtualDtor --analyze DeleteWithNonVirtualDtor.cpp

--- is_trivial_v.cpp
+++ is_trivial_v.cpp
@@ -1,27 +1,91 @@
+#include <ostream>
 #include <type_traits>
+#include <iostream>
 
-struct Inner {
-    int x;
-    void foo() {}  // Member function
-/*
-	Inner() {
-	}
-*/
+struct InnerConstMemberFunction {
+	const int x;
+	void foo() {}
 };
 
-struct Outer {
+struct InnerCtor {
+	int x;
+	InnerCtor() {}
+};
+
+struct Inner {
+	int x;
+};
+
+struct Outer1 {
+    InnerConstMemberFunction inner;
+};
+
+struct Outer2 {
+    InnerCtor inner;
+};
+
+struct Outer3 : Inner {
     Inner inner;
 };
 
-template<typename TTransformType>
-concept ConceptTransformType = std::is_trivially_copyable_v<TTransformType> && std::is_standard_layout_v<TTransformType>;
-// concept ConceptTransformType = std::is_trivial_v<TTransformType>;
-
-template<ConceptTransformType TTransformType>
-void RegisterTransformType() {
-	TTransformType tt;
-}
-
 int main(int argc, char* argv[]) {
-	RegisterTransformType<Outer>();
+    // std::is_trivial equals std::is_trivially_copyable & std::is_trivially_default_constructible
+	std::cout << std::boolalpha 
+			  << "Outer1 is                    trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<Outer1>::value << std::endl
+			  << "Outer2 is                    trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<Outer2>::value << std::endl
+			  << "Outer3 is                    trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<Outer3>::value << std::endl
+			  << "InnerConstMemberFunction is  trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<InnerConstMemberFunction>::value << std::endl
+			  << "InnerCtor is                 trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<InnerCtor>::value << std::endl
+			  << "Inner is                     trivially copy-assignable? "
+			  << std::is_trivially_copy_assignable<Inner>::value << std::endl
+			  << "Outer1 is                    trivial?                   " // underconstrained!
+			  << std::is_trivial<Outer1>::value << std::endl
+			  << "Outer2 is                    trivial?                   "
+			  << std::is_trivial<Outer2>::value << std::endl
+			  << "Outer3 is                    trivial?                   "
+			  << std::is_trivial<Outer3>::value << std::endl
+			  << "Outer1 is                    standard-layout?           "
+			  << std::is_standard_layout<Outer1>::value << std::endl
+			  << "Outer2 is                    standard-layout?           "
+			  << std::is_standard_layout<Outer2>::value << std::endl
+			  << "Outer3 is                    standard-layout?           "
+			  << std::is_standard_layout<Outer3>::value << std::endl
+			  << "Outer1 is                    trivially copyable?        "
+			  << std::is_trivially_copyable<Outer1>::value << std::endl
+			  << "Outer2 is                    trivially copyable?        "
+			  << std::is_trivially_copyable<Outer2>::value << std::endl
+			  << "Outer3 is                    trivially copyable?        "
+			  << std::is_trivially_copyable<Outer3>::value << std::endl
+			  << "Outer1 is                    pod?                       "
+			  << std::is_pod<Outer1>::value << std::endl
+			  << "Outer2 is                    pod?                       "
+			  << std::is_pod<Outer2>::value << std::endl
+			  << "Outer3 is                    pod?                       "
+			  << std::is_pod<Outer3>::value << std::endl;
 }
+
+/*
+Outer1 is                    trivially copy-assignable? false
+Outer2 is                    trivially copy-assignable? true
+Outer3 is                    trivially copy-assignable? true
+InnerConstMemberFunction is  trivially copy-assignable? false
+InnerCtor is                 trivially copy-assignable? true
+Inner is                     trivially copy-assignable? true
+Outer1 is                    trivial?                   true
+Outer2 is                    trivial?                   false
+Outer3 is                    trivial?                   true
+Outer1 is                    standard-layout?           true
+Outer2 is                    standard-layout?           true
+Outer3 is                    standard-layout?           false
+Outer1 is                    trivially copyable?        true
+Outer2 is                    trivially copyable?        true
+Outer3 is                    trivially copyable?        true
+Outer1 is                    pod?                       true
+Outer2 is                    pod?                       false
+Outer3 is                    pod?                       false
+*/
