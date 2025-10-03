b1f9ac6b88ecb757126adb6b0ebfdbb66202c280
Jonas Jensen <jonasje@axis.com>
2025-10-03 15:56:41 +0200

    Reduce SerializeTest.cpp to bare minimum

--- SerializeTest.cpp
+++ SerializeTest.cpp
@@ -1,43 +1,24 @@
 #include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
 #include <sstream>
 #include <vector>
 #include <iostream>
 
 struct Data {
     std::vector<int> values;
-
-    // Tell Boost how to serialize this struct
     template<class Archive>
     void serialize(Archive& ar, const unsigned int version) {
-        ar & values;   // Boost knows how to handle std::vector<int>
+        ar & values;
     }
 };
 
 int main() {
-    // Create object
     Data d1;
     d1.values = {1, 2, 3, 42};
 
-    // Serialize to a string
     std::ostringstream oss;
-    {
-        boost::archive::text_oarchive oa(oss);
-        oa << d1;
-    }
-
+	boost::archive::text_oarchive oa(oss);
+	oa << d1;
     std::string serialized = oss.str();
     std::cout << "Serialized form:\n" << serialized << "\n";
-
-    // Deserialize back
-    Data d2;
-    std::istringstream iss(serialized);
-    {
-        boost::archive::text_iarchive ia(iss);
-        ia >> d2;
-    }
-
-    std::cout << "Deserialized vector: ";
-    for (int v : d2.values) std::cout << v << " ";
-    std::cout << "\n";
 }
