0c3afc2c53e0ec2d2aff29c1090ef2ad765f0fe7
Jonas Jensen <jonasje@axis.com>
2025-10-06 15:54:33 +0200

    Fine-Grained-Locking: Add server buffer writes

--- Fine-Grained-Locking-Client.cpp
+++ Fine-Grained-Locking-Client.cpp
@@ -1,5 +1,6 @@
 #include <boost/asio.hpp>
 #include <boost/asio/buffer.hpp>
+#include <boost/asio/impl/read.hpp>
 #include <boost/asio/impl/write.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
@@ -8,44 +9,73 @@
 #include <boost/interprocess/interprocess_fwd.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/sync/interprocess_upgradable_mutex.hpp>
+#include <boost/io/ios_state.hpp>
+#include <chrono>
 #include <cstring>
 #include <exception>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <unistd.h>
+#include <thread>
+#include <utility>
+#include <vector>
 
 #include "Fine-Grained-Locking-Common.hpp"
 
 namespace bip = boost::interprocess;
 
-int main() {
-    try {
-        boost::asio::io_context io;
-        boost::asio::ip::tcp::socket socket(io);
-		boost::asio::ip::address address = boost::asio::ip::address::from_string("127.0.0.1");
-        socket.connect(boost::asio::ip::tcp::endpoint(address, 12345));
+void readShmData(unsigned char* data) {
+	std::cout << "Client: New data read: ";
+	boost::io::ios_flags_saver ifs(std::cout);
+	for (std::size_t i = 0; i < 10; ++i) {
+		std::cout << "0x" << std::setw(2) << std::setfill('0') << std::hex << +data[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
-		std::string shm_name = "client_sync_" + std::to_string(getpid());
+int main() {
+	try {
+		boost::asio::io_context io;
+		boost::asio::ip::tcp::socket socket(io);
+		boost::asio::ip::address address = boost::asio::ip::address::from_string("127.0.0.1");
+		socket.connect(boost::asio::ip::tcp::endpoint(address, 12345));
+
+		std::stringstream ss;
+		ss << "client_sync_" << std::this_thread::get_id();
+		std::string shm_name = ss.str();
 		bip::shared_memory_object::remove(shm_name.c_str());
 		bip::managed_shared_memory shm(bip::create_only, shm_name.c_str(), 4096);
 		ClientSync* cs = shm.construct<ClientSync>("sync")();
 		std::strncpy(cs->shmName, shm_name.c_str(), shm_name.size() + 1);
-	
+
 		unsigned int size = shm_name.size();
 		boost::asio::write(socket, boost::asio::buffer(&size, 4));
-        boost::asio::write(socket, boost::asio::buffer(shm_name.data(), shm_name.size()));
+		boost::asio::write(socket, boost::asio::buffer(shm_name.data(), shm_name.size()));
 
-		ClientSync *sync = shm.find<ClientSync>("sync").first;
+		boost::asio::read(socket, boost::asio::buffer(&size, sizeof(size)));
+		std::vector<char> buf(size);
+		boost::asio::read(socket, boost::asio::buffer(buf.data(), buf.size()));
+		std::string srvShm = std::string(buf.begin(), buf.end());
+		std::cout << "Client: The server has sent us its shared memory name: " << srvShm << std::endl;
+
+		bip::managed_shared_memory region(bip::open_only, srvShm.c_str());
+		std::pair<unsigned char*, std::size_t> p = region.find<unsigned char>("server buffer");
+		unsigned char* data						 = p.first;
+
 		while (true) {
 			{
-				bip::sharable_lock<bip::interprocess_upgradable_mutex> lock(sync->mutex);
-				std::cout << "Client " << getpid() << " reading shared data...";
-				sleep(1);
+				bip::sharable_lock<bip::interprocess_upgradable_mutex> lock(cs->mutex);
+				std::cout << "Client " << std::this_thread::get_id() << " reading shared data..."
+						  << std::endl;
+				readShmData(data);
+				std::this_thread::sleep_for(std::chrono::seconds(1));
 			}
 			std::cout << " done" << std::endl;
-			sleep(1);
+			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
-	} catch (std::exception &e) {
+	}
+	catch (std::exception& e) {
 		std::cerr << "Client error: " << e.what() << "\n";
 	}
 }
--- Fine-Grained-Locking-Server.cpp
+++ Fine-Grained-Locking-Server.cpp
@@ -1,6 +1,7 @@
 #include <boost/asio.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/impl/read.hpp>
+#include <boost/asio/impl/write.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/interprocess/creation_tags.hpp>
@@ -8,18 +9,34 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/offset_ptr.hpp>
 #include <boost/interprocess/sync/interprocess_upgradable_mutex.hpp>
+#include <boost/io/ios_state.hpp>
 #include <chrono>
+#include <cstddef>
 #include <cstdint>
 #include <exception>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <thread>
-#include <unistd.h>
 #include <vector>
 
 #include "Fine-Grained-Locking-Common.hpp"
 
 namespace bip = boost::interprocess;
+
+void updateShmData(unsigned char* data) {
+	for (std::size_t i = 0; i < 10; ++i) {
+		data[i] = data[i] + 1;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+	std::cout << "Server: New data written: ";
+	boost::io::ios_flags_saver ifs(std::cout);
+	for (std::size_t i = 0; i < 10; ++i) {
+		std::cout << "0x" << std::setw(2) << std::setfill('0') << std::hex << +data[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 int main() {
 	try {
@@ -29,17 +46,30 @@ int main() {
 		boost::asio::ip::tcp::socket socket(io);
 		std::vector<std::string> clientShmNames;
 
+		std::stringstream ss;
+		ss << "server_buffer_" << std::this_thread::get_id();
+		std::string shm_name = ss.str();
+		bip::shared_memory_object::remove(shm_name.c_str());
+		bip::managed_shared_memory shm(bip::create_only, shm_name.c_str(), 4096);
+		std::cout << "Shared memory address: " << shm.get_address() << ", size: " << shm.get_size()
+				  << std::endl;
+		unsigned char* data = shm.construct<unsigned char>("server buffer")[10](0x55);
+
 		for (int i = 0; i < 2; i++) {
 			acceptor.accept(socket);
 
 			uint32_t msg_size = 0;
 			boost::asio::read(socket, boost::asio::buffer(&msg_size, sizeof(msg_size)));
-	
+
 			std::vector<char> buf(msg_size);
 			boost::asio::read(socket, boost::asio::buffer(buf.data(), buf.size()));
 
 			std::string& shmName = clientShmNames.emplace_back(buf.begin(), buf.end());
 			std::cout << "Server: A client has sent us its shared memory name: " << shmName << std::endl;
+
+			unsigned int size = shm_name.size();
+			boost::asio::write(socket, boost::asio::buffer(&size, 4));
+			boost::asio::write(socket, boost::asio::buffer(shm_name.data(), shm_name.size()));
 
 			socket.close();
 		}
@@ -53,16 +83,16 @@ int main() {
 
 		while (true) {
 			std::cout << "Server: Taking exclusive locks before modifying the shared buffer: ";
-
 			std::vector<bip::scoped_lock<bip::interprocess_upgradable_mutex>> locks;
 			for (bip::offset_ptr<ClientSync> s : syncObjects) {
 				locks.emplace_back(s->mutex);
 				std::cout << s->shmName << " ";
+				break;
 			}
 			std::cout << std::endl;
 
 			std::cout << "Server: Doing work" << std::endl;
-			std::this_thread::sleep_for(std::chrono::seconds(5));
+			updateShmData(data);
 
 			std::cout << "Server: Releasing all client locks" << std::endl;
 			locks.clear();
@@ -70,7 +100,8 @@ int main() {
 			std::cout << "Server: Idle" << std::endl;
 			std::this_thread::sleep_for(std::chrono::seconds(5));
 		}
-	} catch (std::exception &e) {
+	}
+	catch (std::exception& e) {
 		std::cerr << "Server error: " << e.what() << std::endl;
 	}
 }
